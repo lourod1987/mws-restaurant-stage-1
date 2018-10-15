635ad848ccb511370ae8b05b3b6689a84c399a9e
lourod1987 <louart87@gmail.com>
2018-10-15 00:02:00 -0500

    changed alt to use variables to generate more descriptive alt text.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -73,7 +73,8 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
     const image = document.getElementById('restaurant-img');
     image.className = 'restaurant-img';
     image.src = DBHelper.imageUrlForRestaurant(restaurant);
-    image.alt = 'Restaurant pictured.';
+    image.alt = `${restaurant.name} restaurant in ${restaurant.neighborhood}.`;
+    
     const cuisine = document.getElementById('restaurant-cuisine');
     cuisine.innerHTML = restaurant.cuisine_type;
 
